51861cd088fc5644a1bb711e8f5e27a53643ab4d
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2025-03-21 11:32:09 -0500

    fix: .mjs rename incomplete

--- mkdocs_tacc/tacc_readthedocs/js/tacc-theme.module.js
+++ mkdocs_tacc/tacc_readthedocs/js/tacc-theme.module.js
@@ -3,5 +3,5 @@ import './tacc-theme/addPageNameToRoot.js';
 import './tacc-theme/autoScrollNavOnPageReload.js';
 import './tacc-theme/redirectNavLinks.js';
 import './tacc-theme/removeSectionClass.js';
-import './tacc-theme/setTargetForExternalLinks.mjs';
-import './tacc-theme/swapImgSvgWithRawSvg.mjs';
+import './tacc-theme/setTargetForExternalLinks.module.js';
+import './tacc-theme/swapImgSvgWithRawSvg.module.js';
